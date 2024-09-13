0bfe8d5e8af8af1fed394382e28ef9cc5bea03c2
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-09-13 14:50:35 -0300

    Selects numerados - Select 6 reajustado

--- SQL-Exercicios/Exercicio10.sql
+++ SQL-Exercicios/Exercicio10.sql
@@ -191,43 +191,40 @@ VALUES
 
 
 
---Liste todas as linhas e os campos cd_Comprador, nm_Comprador e ds_Email da tabela COMPRADOR 
+--1.Liste todas as linhas e os campos cd_Comprador, nm_Comprador e ds_Email da tabela COMPRADOR 
 SELECT cd_comprador, nm_comprador, ds_email
 FROM tb_comprador
 
---Liste todas as linhas e os campos cd_Vendedor, nm_Vendedor e ds_Email da tabela VENDEDOR em ordem alfabética decrescente. 
-SELECT cd_vendedor, nm_vendedor, ds_endereco
+--2.Liste todas as linhas e os campos cd_Vendedor, nm_Vendedor e ds_Email da tabela VENDEDOR em ordem alfabética decrescente. 
+SELECT cd_vendedor, nm_vendedor, ds_email
 FROM tb_vendedor
 ORDER BY nm_vendedor DESC
 
---Liste as colunas cd_Imovel, cd_Vendedor e vl_Preco de todos os imóveis do vendedor 2
+--3.Liste as colunas cd_Imovel, cd_Vendedor e vl_Preco de todos os imóveis do vendedor 2
 SELECT cd_imovel, cd_vendedor, vl_preco
 FROM tb_imovel
 WHERE cd_vendedor = 2
 
---Liste as colunas cd_Imovel, cd_Vendedor, vl_Preco e sg_Estado dos imóveis cujo preço de venda seja inferior a 150 mil e sejam do Estado do RJ. 
+--4.Liste as colunas cd_Imovel, cd_Vendedor, vl_Preco e sg_Estado dos imóveis cujo preço de venda seja inferior a 150 mil e sejam do Estado do RJ. 
 SELECT cd_imovel, cd_vendedor, vl_preco, sg_estado
 FROM tb_imovel
 WHERE vl_preco < 150000 AND sg_estado = 'RJ'
 
---Liste as colunas cd_Imovel, cd_Vendedor, vl_Preco e sg_Estado dos imóveis cujo preço de venda seja inferior a 150 mil e o vendedor não seja 2. 
+--5.Liste as colunas cd_Imovel, cd_Vendedor, vl_Preco e sg_Estado dos imóveis cujo preço de venda seja inferior a 150 mil e o vendedor não seja 2. 
 SELECT cd_imovel, cd_vendedor, vl_preco, sg_estado
 FROM tb_imovel
 WHERE vl_preco < 150000 AND cd_vendedor != 2
 
---Liste as colunas cd_Comprador, nm_Comprador, ds_Endereco e sg_Estado da tabela COMPRADOR em que o Estado seja nulo.
+--6.Liste as colunas cd_Comprador, nm_Comprador, ds_Endereco e sg_Estado da tabela COMPRADOR em que o Estado seja nulo.
 SELECT cd_comprador, nm_comprador, ds_endereco, sg_estado
 FROM tb_comprador
-WHERE NOT EXISTS (
-	SELECT 1
-	FROM tb_estado
-	WHERE tb_comprador.sg_estado = tb_estado.sg_estado)
+WHERE sg_estado IS NULL
 
---Liste todas as ofertas cujo valor esteja entre 100 mil e 150 mil.
+--7.Liste todas as ofertas cujo valor esteja entre 100 mil e 150 mil.
 SELECT * FROM tb_oferta
 WHERE vl_oferta > 100000 AND vl_oferta < 150000
 
---Liste todas as ofertas cuja data da oferta esteja entre 01/01/2009 e 01/03/2009.
+--8.Liste todas as ofertas cuja data da oferta esteja entre 01/01/2009 e 01/03/2009.
 SELECT * FROM tb_oferta
 WHERE dt_oferta >= '2009-01-01' AND dt_oferta <= '2009-03-01'
 
@@ -235,7 +232,7 @@ WHERE dt_oferta >= '2009-01-01' AND dt_oferta <= '2009-03-01'
 --SELECT * FROM tb_oferta
 --WHERE dt_oferta >= CONVERT(DATE, '01/01/2009', 103) AND dt_oferta <= CONVERT(DATE, '01/03/2009', 103);
 
---Liste todos os vendedores que comecem com a letra M.
+--9.Liste todos os vendedores que comecem com a letra M.
 SELECT * FROM tb_vendedor
 WHERE nm_vendedor LIKE 'M%'
 
@@ -245,37 +242,37 @@ WHERE nm_vendedor LIKE 'M%'
 --	[ ]: Define um intervalo ou uma lista de caracteres.
 --	[^]: Exclui caracteres (encontra qualquer caractere que não esteja na lista).
 
---Liste todos vendedores que tenham a letra A na segunda posição do nome
+--10.Liste todos vendedores que tenham a letra A na segunda posição do nome
 SELECT * FROM tb_vendedor
 WHERE nm_vendedor LIKE '_A%'
 
---Liste todos os compradores que tenham a letra U em qualquer posição do endereço 
+--11.Liste todos os compradores que tenham a letra U em qualquer posição do endereço 
 SELECT * FROM tb_comprador
 WHERE ds_endereco LIKE '%[U]%'
 
---Liste todos os imóveis cujo código seja 2 ou 3 em ordem alfabética de endereço.
+--12.Liste todos os imóveis cujo código seja 2 ou 3 em ordem alfabética de endereço.
 SELECT * FROM tb_imovel
 WHERE cd_imovel = 2 OR cd_imovel = 3
 ORDER BY ds_endereco ASC
 
---Liste todas as ofertas cujo imóvel seja 2 ou 3 e o valor da oferta seja maior que 140 mil, em ordem decrescente de data.
+--13.Liste todas as ofertas cujo imóvel seja 2 ou 3 e o valor da oferta seja maior que 140 mil, em ordem decrescente de data.
 SELECT * FROM tb_oferta
 WHERE (cd_imovel = 2 OR cd_imovel = 3) AND vl_oferta > 140000 --O operador AND tem maior precedência que o OR, então é preciso agrupar as condições com ()
 ORDER BY dt_oferta DESC
 
---Liste todos os imóveis cujo preço de venda esteja entre 110 mil e 200 mil ou seja do vendedor 4 em ordem crescente de área útil.
+--14.Liste todos os imóveis cujo preço de venda esteja entre 110 mil e 200 mil ou seja do vendedor 4 em ordem crescente de área útil.
 SELECT * FROM tb_imovel
 WHERE vl_preco > 110000 AND vl_preco < 200000 OR cd_vendedor = 4
 ORDER BY qt_areaUtil ASC
 
---Verifique a maior, a menor e o valor médio das ofertas desta tabela.
+--15.Verifique a maior, a menor e o valor médio das ofertas desta tabela.
 SELECT MAX(vl_oferta) FROM tb_oferta
 
 SELECT MIN(vl_oferta) FROM tb_oferta
 
 SELECT AVG(vl_oferta) AS 'Valor Médio das Ofertas' FROM tb_oferta
 
---Mostre o maior, o menor, o total e a média de preço de venda dos imóveis.
+--16.Mostre o maior, o menor, o total e a média de preço de venda dos imóveis.
 SELECT MAX(vl_preco) FROM tb_imovel
 
 SELECT MIN(vl_preco) FROM tb_imovel
@@ -284,7 +281,7 @@ SELECT SUM (vl_preco) AS 'Total de Preços de Venda' FROM tb_imovel
 
 SELECT AVG(vl_preco) 'Valor Médio de Preços de Venda' FROM tb_imovel
 
---Faça uma busca que retorne o total de ofertas realizadas nos anos de 2008, 2009 e 2010. 
+--17.Faça uma busca que retorne o total de ofertas realizadas nos anos de 2008, 2009 e 2010. 
 SELECT COUNT(*) FROM tb_oferta
 WHERE dt_oferta > '2008-01-01' AND dt_oferta < '2010-01-01'
 
