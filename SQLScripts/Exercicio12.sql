a81c0f18896fbc8135e92476cc01007769544982
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-10-17 14:22:57 -0300

    Corrigido select 1

--- SQLScripts/Exercicio12.sql
+++ SQLScripts/Exercicio12.sql
@@ -1,19 +1,17 @@
 --1. Faça uma lista de imóveis do mesmo bairro do imóvel 2. Exclua o imóvel 2 da sua busca. 
 SELECT
-    cd_imovel AS "Código do imóvel",
-    nm_bairro
+    cd_imovel AS 'Código do imóvel'
 FROM
-    tb_imovel i
-JOIN tb_bairro b ON i.cd_bairro = b.cd_bairro
-    AND i.cd_cidade = b.cd_cidade
-    AND i.sg_estado = b.sg_estado
-WHERE 
-    b.cd_bairro = (
-        SELECT cd_bairro
-        FROM tb_imovel 
-        WHERE cd_imovel = 2
-    )
-AND cd_imovel <> 2;
+    tb_imovel i,(
+        select  cd_bairro, cd_cidade, sg_estado
+        from tb_imovel
+        where cd_imovel = 2
+        ) imovel2
+WHERE
+    i.cd_bairro = imovel2.cd_bairro
+    AND i.cd_cidade = imovel2.cd_cidade
+    AND i.sg_estado = imovel2.sg_estado
+    AND i.cd_imovel <> 2;
 
 --2.Faça uma lista que mostre todos os imóveis que custam mais que a média de preço dos imóveis. 
 SELECT
